c1a8cdf4faae4d0db9f65c4cec5c877e547863a7
jnsrikanth <srikanth@technovature.com>
2020-02-11 18:19:54 +0530

    Adding a Hyperlink to the Item

--- src/components/gallery/item/item.js
+++ src/components/gallery/item/item.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Img from 'gatsby-image';
+import { Link } from gatsby;
 import { Title, Copy } from './item.css';
 
 const Item = ({ title, copy, image }) => (
   <figure>
+  <Link to="/" style={{ textDecoration: 'none' }}>
     <Img fluid={image ? image.childImageSharp.fluid : {}} alt={title} />
+  </Link>
     <figcaption>
       <Title>{title}</Title>
       <Copy>{copy}</Copy>
