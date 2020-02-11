b8d2b577db38e138b14806f2eb68d2656e2834b1
jnsrikanth <srikanth@technovature.com>
2020-02-11 17:02:15 +0530

    Added Hyperlinks to each gallery item making it clickable.

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Item from 'components/gallery/item';
+import { Link } from 'gatsby';
 import { Container } from './gallery.css';
 
 const Gallery = ({ items }) => (
   <Container>
     {items.map((item, i) => (
-      <Item {...item} key={i} />
+    <Link to="/">  <Item {...item} key={i} /> </Link>
     ))}
   </Container>
 );
