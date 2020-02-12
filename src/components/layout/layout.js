5266afe3100b36c1f27696d183a4ed51800faba3
jnsrikanth <srikanth@technovature.com>
2020-02-12 19:07:49 +0530

    Commented GlobalStyles temporarily

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { StaticQuery, graphql } from 'gatsby';
 import Head from 'components/head';
 import Header from 'components/header';
-import GlobalStyle from 'global.css.js';
+//import GlobalStyle from 'global.css.js';
 import favicon from '../images/favicon.ico'
 import Helmet from 'react-helmet'
 
