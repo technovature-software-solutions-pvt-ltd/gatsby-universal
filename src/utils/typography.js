25b42ccbf1fa1f7fa1e5a978b780a07718626a83
jnsrikanth <srikanth@technovature.com>
2020-02-13 09:25:59 +0530

    Updated body font to Roboto sans-serif

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -11,7 +11,7 @@ const typography = new Typography({
     "Arial",
     "sans-serif",
   ],
-  bodyFontFamily: ["Georgia", "serif"],
+  bodyFontFamily: ["Roboto", "sans-serif"],
 })
 
 export default typography
