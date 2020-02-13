1131eac807a131a4e577ad8b288c103674e949e3
jnsrikanth <srikanth@technovature.com>
2020-02-13 11:22:53 +0530

    Update item.css.js

--- src/components/gallery/item/item.css.js
+++ src/components/gallery/item/item.css.js
@@ -3,7 +3,7 @@ import MEDIA from 'helpers/mediaTemplates';
 
 export const Title = styled.span`
   display: block;
-  font-size: 1.4rem;
+  font-size: 1.5rem;
   font-weight: 500;
   margin: 2rem 2rem 1rem;
 `;
