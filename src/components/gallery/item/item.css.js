24245a4615ade306ca295b8557eda0b5c2899f77
jnsrikanth <srikanth@technovature.com>
2020-02-13 11:50:16 +0530

    margins were too high - reduced it
    reduced 2rem to 1rem, 1rem to 0.5rem

--- src/components/gallery/item/item.css.js
+++ src/components/gallery/item/item.css.js
@@ -5,12 +5,12 @@ export const Title = styled.span`
   display: block;
   font-size: 1.5rem;
   font-weight: 500;
-  margin: 2rem 2rem 1rem;
+  margin: 1rem 1rem 0.5rem;
 `;
 
 export const Copy = styled.p`
   color: #757575;
-  margin: 0 2rem 2rem;
+  margin: 0 1rem 1rem;
 
   ${MEDIA.TABLET`
     margin-bottom: 4rem;
