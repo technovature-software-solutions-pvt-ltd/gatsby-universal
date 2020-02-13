f5b4ae7e478fb8fcfcc88e34441d22e4ecfc2447
jnsrikanth <srikanth@technovature.com>
2020-02-13 07:42:26 +0530

    Update nav.js

--- src/components/header/nav/nav.js
+++ src/components/header/nav/nav.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import { Container } from './nav.css';
+import Helmet from 'react-helmet'
+
 
 const Nav = () => (
   <Container>
+  <Helmet>
+  <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+  </Helmet>
     <ul>
       <li>
         <Link to="/">Home</Link>
